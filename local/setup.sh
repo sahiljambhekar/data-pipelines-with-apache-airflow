53298baca7946aaf7d16363abe8c944df95d523e
Sahil Jam <sahiljambhekar@github.com>
2022-02-10 17:50:11 -0500

    Fix local/setup.sh for Apple M1.

--- local/setup.sh
+++ local/setup.sh
@@ -1,13 +1,19 @@
-#!/bin/zsh
-cd ..
+#!/bin/sh
 source ../venv/bin/activate
+alias python3=$(which python3)
+
 AIRFLOW_VERSION=2.2.3
 
 export AIRFLOW_HOME=~/airflows/2.2
-PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+PYTHON_VERSION="$(python3 --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
 # For example: 3.8
 CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
-pip install -r requirements.txs --constraint "${CONSTRAINT_URL}"
+NUMPY_VERSION=$(curl -sL -XGET $CONSTRAINT_URL | grep numpy)
+echo "${CONSTRAINT_URL} ${NUMPY_VERSION}"
+# https://tilcode.blog/2021/06/14/how-to-install-numpy-and-pandas-for-data-science-in-a-m1-macbook/
+pip3 install Cython
+pip3 install --no-binary :all: --no-use-pep517 ${NUMPY_VERSION}
+pip3 install -r ../requirements.txt --constraint "${CONSTRAINT_URL}"
 # https://airflow.apache.org/docs/apache-airflow/1.10.3/installation.html
 
 airflow standalone
